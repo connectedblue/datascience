1b3c987fc7e7df109a6daeeab7a8dbd9543d6ba7
connectedblue <connectedblue@users.noreply.github.com>
2016-05-28 06:30:21 +0100

    update

--- gtrends/test.R
+++ gtrends/test.R
@@ -7,5 +7,5 @@ x<-gtrends(c("How to smoke salmon"), c("US-OR", "US-AK"), type="trends")
 
 df<-x$trend
 
-mean_or<-mean(df[3], na.rm = T)
-mean_ak<-mean(df[4], na.rm = T)
+mean_or<-mean(df[,3], na.rm = T)
+mean_ak<-mean(df[,4], na.rm = T)
