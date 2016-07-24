b95a40929c9a91c57c90f708b8e0e6b5d85b4fb6
connectedblue <connectedblue@users.noreply.github.com>
2016-07-24 17:43:25 +0100

    Started writing interim report for project - this version compiles and contains all the basic analysis objects

--- machine_learning/pmlproject/src/rf-model-caret.R
+++ machine_learning/pmlproject/src/rf-model-caret.R
@@ -2,28 +2,29 @@ library('ProjectTemplate')
 load.project()
 
 
+## ---- analysis
+
 set.seed(1234)
 # Run the model on all four cores
 registerDoParallel(cores=4)
 
-# Do a number of trees on each cpu and combine them
-caretrf_mod <- train(classe~., data=training, 
-             trControl=trainControl(method="cv",number=5),
-             method="parRF")
+# if the model is not already present, train one using cross validation
+if(!exists("caretrf_mod")){
+        caretrf_mod <- train(classe~., data=training, 
+                     trControl=trainControl(method="cv",number=5),
+                     method="parRF")
+
+}
               
 
 # Run the model against the testing set
-caretrf_testing_prediction<-predict(mod$finalModel, testing)
-caretrf_testing_result<-confusionMatrix(testing_prediction, testing$classe)
+caretrf_testing_prediction<-predict(caretrf_mod$finalModel, testing)
+caretrf_testing_result<-confusionMatrix(caretrf_testing_prediction, testing$classe)
 
 
 # Get the answers on the clean testing set to submit to the quiz
-caretrf_submitted_answers<-predict(mod, clean_testing)
+caretrf_submitted_answers<-predict(caretrf_mod, clean_testing)
 
-# Save key results in the cache (because it takes a while to run)
+# Save the model in the cache (because it takes a while to run)
 
 cache("caretrf_mod")
-cache("caretrf_testing_prediction")
-cache("caretrf_testing_result")
-cache("caretrf_submitted_answers")
-
