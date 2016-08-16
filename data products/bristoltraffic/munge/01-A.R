706a5b25fecdd12dcce0c05d68c387d31cfaa16d
connectedblue <connectedblue@users.noreply.github.com>
2016-08-16 23:51:15 +0100

    added some data correction fixes

--- data products/bristoltraffic/munge/01-A.R
+++ data products/bristoltraffic/munge/01-A.R
@@ -22,10 +22,33 @@ if(!exists("journeys")){
         journeys$day <- weekdays(journeys$time)
         journeys$month <- months(journeys$time)
         journeys$week <- as.numeric(strftime(journeys$time, "%W"))
+        journeys$hour <- as.numeric(strftime(journeys$time, "%H"))
+        journeys$distance <- round(journeys$travel.time/(60*60)*journeys$est_speed,1)
+
+        # fix errors in the data, found through exploratory analysis
+        
+        # some sections have 2 leading zeros instead of 3
+        journeys$section_id <- sub("SECTIONTL00([0-9])([0-9])$", "SECTIONTL000\\1\\2", 
+                                   journeys$section_id)
+        
+        # this section has the wrong section ID - should be 176, not 130
+        journeys$section_id <-ifelse(journeys$section_id=="SECTIONTL00130" & 
+                                     journeys$section_description=="St Michaels OB to Blackboy Hill OB",
+                                     "SECTIONTL00176", journeys$section_id )
+        
+        # these sections have the wrong distance - corrected to the majority for the route
+        journeys$distance <-ifelse(journeys$section_id=="SECTIONTL00104" &
+                                   journeys$distance==1.8, 2.8, journeys$distance )
+        journeys$distance <-ifelse(journeys$section_id=="SECTIONTL00109" &
+                                           journeys$distance==1.7, 2.8, journeys$distance )
         
 }
 
 # cache and tidy up
 update_cache(c("Historic.journey.times", "journeys"))
 rm(Historic.journey.times)
+
+summary <- journeys %>% group_by(section_id, section_description,
+                                 distance) %>%
+                     summarise(n=n())
 
